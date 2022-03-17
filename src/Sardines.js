f65ac622314ca63dd0fee749771354055fa8b175
sanamcha <mhr.sanam@gmail.com>
2022-03-17 16:54:55 -0700

    added sardines component

---
+++ src/Sardines.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Sardines = () => {
+    return (
+        <div 
+        className = "Sardines"
+        style = {{backgroundImage:"url(https://media.giphy.com/media/tVk4w6EZ7eGNq/giphy.gif)"}}> 
+        <h1>YOU DON'T EAT THE SARDINES. THE SARDINES, THEY EAT YOU.</h1>  
+       <h1><Link to="/">Go Back</Link></h1>
+            
+        </div>
+    );
+}
+
+export default Sardines;
