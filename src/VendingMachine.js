7623935f333b6ef7cb53d12e69bb5a41be06eb3c
sanamcha <mhr.sanam@gmail.com>
2022-03-17 16:38:15 -0700

    added chips in vending machine

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -7,14 +7,13 @@ import vendingMachineImg from './images/VendingMachine.png';
 const VendingMachine = () => {
     return (
         <div className='VendingMachine'>
-            <img src={vendingMachineImg} />
+            <img src={vendingMachineImg} alt="vending machine"/>
             <h1>Hello I am vending Machine. What would you like to eat?</h1>
             <h1><Link to='/soda'>Soda</Link></h1>
+            <h1><Link to='/chips'>Chips</Link></h1>
         </div>
-    )
+    );
 }
-
-
 
 
 export default VendingMachine;
