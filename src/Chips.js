28d151e244a0e84efd3aa952098c5323ccb6a331
sanamcha <mhr.sanam@gmail.com>
2022-03-17 16:37:45 -0700

    added chips

---
+++ src/Chips.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import chipsImage from "./images/Chips.png";
+
+
+const Chips = () => {
+    const [packets, setPackets] = useState([]);
+    
+    function handleClick() {
+        const x = window.innerWidth * Math.random();
+        const y = window.innerHeight * Math.random();
+        setPackets(p => [...p, {x, y}]);
+    }
+
+    const bags = packets.map((packet, i) => (
+        <img 
+            key={i}
+            src = {chipsImage}
+            className ="packet"
+            style = {{top: `${packet.y}px`, left: `${packet.x}px`}}
+            alt="packets of lay's chips"/>
+    ));
+    return (
+        <div className="Chips">
+            <h1>Chips packets eaten: {packets.length}</h1> 
+            <button onClick={handleClick}>NOM NOM NOM</button> 
+            <h1>
+                <Link to='/'>Go Back</Link>
+            </h1>
+              
+            {bags}
+        </div>
+      
+    );
+}
+
+export default Chips;
