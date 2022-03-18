7144611ef74a9d63dfdaaad706e0d5ecb7946a67
sanamcha <mhr.sanam@gmail.com>
2022-03-17 17:49:22 -0700

    final update

--- src/Soda.js
+++ src/Soda.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import sodaImage from "./images/Soda.png";
+import './Soda.css'
 
 const Soda = () => {
     return (
         <div className="Soda">
-            <img src={sodaImage} />
-            <h1>OMG SUGARRRR</h1>
+            <img src={sodaImage} alt="cane coke soda"/>
+            <h1>OMG SUGARRRR</h1> 
             <h1><Link to='/'>Go Back</Link></h1>
             <img src ={sodaImage} />
+    
         </div>
-        
     )
 }
 
--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import vendingMachineImg from './images/VendingMachine.png';
-
+import './VendingMachine.css';
  
 
 const VendingMachine = () => {
     return (
-        <div className='VendingMachine'>
-            <img src={vendingMachineImg} alt="vending machine"/>
+        <div className='VendingMachine'
+        style={{backgroundImage:`url(${vendingMachineImg})` }}
+        >
+            
             <h1>HELLO I AM VENDING MACHINE. WHAT WOULD YOU LIKE TO EAT?</h1>
             <h1><Link to='/soda'>SODA</Link></h1>
             <h1><Link to='/chips'>CHIPS</Link></h1>
